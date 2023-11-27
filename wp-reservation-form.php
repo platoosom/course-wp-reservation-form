995c5f3bb01f77a567a77b8ae8a17fb71fdc3b7a
platoosom <platoosom@gmail.com>
2023-11-27 09:44:58 +0700

    เขียนหัวปลักอิน

---
+++ wp-reservation-form.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * Plugin Name:     WP Reservation Form
+ * Plugin URI:      http://boostpress.com         
+ * Description:     Using for reservation fom
+ * Version:         0.1            
+ * Requires at least: 6.4.1 
+ * Requires PHP:    8.0 
+ * Author:          Watcharamet Chitsanukup Srinethiroth  
+ * Author URI:      http://select2web.com
+ */
+
+ class WP_Reservation_Form 
+ {
+
+ }
+
+ new WP_Reservation_Form();
+ 
