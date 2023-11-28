52b9494b8328e8a6d2b37d4f7cbc57ef86ebb1e8
platoosom <platoosom@gmail.com>
2023-11-28 18:27:13 +0700

    การสร้าง shortcode

--- wp-reservation-form.php
+++ wp-reservation-form.php
@@ -15,7 +15,55 @@
  {
     public function __construct()
     {
+        // Register shortcode
+        add_action('init', array($this, 'register_shortcode'));
+    }
 
+
+    public function register_shortcode()
+    {
+        add_shortcode('reservation-form', array($this, 'reservation_form_content'));
+    }
+
+
+    public function reservation_form_content()
+    {
+        ob_start();
+        ?>
+            <form class="uk-form-stacked" method="post" action="">
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="fullname">Fullname</label>
+                    <div class="uk-form-controls">
+                        <input name="fullname" class="uk-input" id="fullname" type="text" placeholder="Fullname">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="phone">Phone</label>
+                    <div class="uk-form-controls">
+                        <input name="phone" class="uk-input" id="phone" type="text" placeholder="Phone">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="date">Date</label>
+                    <div class="uk-form-controls">
+                        <input name="date" class="uk-input" id="date" type="date" placeholder="Date">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="detail">Detail</label>
+                    <div class="uk-form-controls">
+                        <textarea  name="detail" class="uk-textarea" id="detail" placeholder="Detail"></textarea>
+                    </div>
+                </div>
+            </form>
+
+        <?php 
+        $content = ob_get_clean();
+        return $content;
     }
 
  }
